c0c60130458a65c8f84c3ac022ba19e2c89b8d4f
Takizawa Hiroki <owner@th2.info>
2018-10-27 16:02:13 +0900

    Introduce vuetify.

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Site Title',
+  theme: 'vuetifipress',
+  head: [
+    ['link', {
+      href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      rel: 'stylesheet'
+    }]
+  ]
+}
---
+++ vuetifipress/layouts/Layout.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-toolbar app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>{{this.$siteTitle}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app fixed>
+      <v-list dense>
+        <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+                @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <v-container fluid fill-height>
+        <Content/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "Layout",
+    data () {
+      return {
+        drawer: false,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
+      }
+    }
+  }
+</script>
